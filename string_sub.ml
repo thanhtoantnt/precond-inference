022c320809d3dae4dfbed45f609a71f0e238b225
thanhtoantnt <thanhtoantnt@gmail.com>
2017-10-20 11:37:22 +0800

    polynomial2

--- string_sub.ml
+++ string_sub.ml
@@ -8,9 +8,9 @@ let passing =
     ~name:"string get example"
     (QCheck.triple QCheck.printable_string QCheck.small_nat QCheck.small_nat)
     (fun (a, b, c) ->
-      let a = String.map (fun x -> if x = '\n' then 'x' else x) a in
-      let a = String.map (fun x -> if x = ' ' then 'x' else x) a in
-      let a = String.map (fun x -> if x = ',' then 'x' else x) a in
+      let a = String.map (fun x -> if (x = '\n') || (x = '(') || (x = ')') then 'a' else x) a in
+      let a = String.map (fun x -> if x = ' ' then 'b' else x) a in
+      let a = String.map (fun x -> if x = ',' || x = '{' || x = '}' then 'c' else x) a in
       let () = print_string ("(" ^ (a) ^ ","  ^ (string_of_int b) ^ "," ^ (string_of_int c) ^  ")")  in
       if ((b < 0) || (c < 0) || (b > (String.length a) - c )) then
         let () = print_string(", F") in
